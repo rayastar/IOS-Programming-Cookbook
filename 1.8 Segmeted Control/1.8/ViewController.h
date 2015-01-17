80fda685a65d775735febe0bae699e345a1eab1c
Marat <maratik93@gmail.com>
2015-01-17 13:10:45 +0500

    1.8

---
+++ 1.8 Segmeted Control/1.8/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  1.8
+//
+//  Created by marat on 06/01/2015.
+//  Copyright (c) 2015 marat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (nonatomic, strong) UISegmentedControl *mySegmentedControl;
+@end
---
+++ 1.8 Segmeted Control/1.8/ViewController.m
@@ -0,0 +1,62 @@
+//
+//  ViewController.m
+//  1.8
+//
+//  Created by marat on 06/01/2015.
+//  Copyright (c) 2015 marat. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+    NSArray *segments = [[NSArray alloc] initWithObjects:
+                         @"iPhone",
+                         @"iPad",
+                         @"iPod",
+                         @"iMac", nil];
+    
+//    self.mySegmentedControl = [[UISegmentedControl alloc]
+//                               initWithItems:segments];
+//    self.mySegmentedControl.center = self.view.center;
+//    [self.view addSubview:self.mySegmentedControl];
+//    self.mySegmentedControl.transform = CGAffineTransformMakeRotation(M_PI / 2.0);
+    
+   
+    
+    self.mySegmentedControl = [[UISegmentedControl alloc]
+                               initWithItems:segments];
+    self.mySegmentedControl.center = self.view.center;
+    [self.view addSubview:self.mySegmentedControl];
+    //rself.mySegmentedControl.transform = CGAffineTransformMakeRotation(M_PI / 3.0);
+    [self.mySegmentedControl addTarget:self action:@selector(segmentChanged:)
+                      forControlEvents:UIControlEventValueChanged];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    
+}
+
+- (void) segmentChanged:(UISegmentedControl *)paramSender
+{
+    if ([paramSender isEqual:self.mySegmentedControl])
+    {
+        NSInteger selectedSegmentIndex = [paramSender selectedSegmentIndex];
+        NSString  *selectedSegmentText =
+        [paramSender titleForSegmentAtIndex:selectedSegmentIndex];
+        NSLog(@"Segment %ld with %@ text is selected", (long)selectedSegmentIndex, selectedSegmentText);
+    }
+}
+
+@end
